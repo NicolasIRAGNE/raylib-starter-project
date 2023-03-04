a20119f7d02d9369b80c9c23b3587f792094eabe
Nicolas IRAGNE <nicolas.iragne@alyce.fr>
2023-03-04 22:16:07 +0100

    add some useless stuff

---
+++ src/foo.h
@@ -0,0 +1,20 @@
+/**
+ * @file foo.h
+ * @author Nicolas IRAGNE (nicolas.iragne@alyce.fr)
+ * @brief Test function to check if includes are working
+ * @date 2023-03-04
+ *
+ * @copyright Copyright Alyce (c) 2023
+ */
+
+#pragma once
+
+#include <stdio.h>
+
+/**
+ * @brief Prints "foo" to the console. Yay!
+ */
+inline void foo()
+{
+    printf("foo\n");
+}
--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
+/**
+ * @file main.c
+ * @author Nicolas IRAGNE (nicolas.iragne@alyce.fr)
+ * @brief Entry point of the program
+ * @date 2023-03-04
+ *
+ * @copyright Copyright Alyce (c) 2023
+ */
+
 #include <raylib.h>
+#include "foo.h"
 #include <stdio.h>
 
 int main()
 {
+    foo();
     InitWindow(800, 450, "Hello World!");
     while (!WindowShouldClose())
     {
